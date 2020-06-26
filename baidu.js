0630c0b530dca2269c076d8be71e8c69e0de9506
zy <zmouren@gmail.com>
2020-06-26 16:37:24 +0800

    add baidu tongji

---
+++ baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1fede1ee211d178cb0746250fb1189c0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
